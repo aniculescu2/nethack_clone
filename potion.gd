14e9dd3d8b32a2f4c6e0f6be54d6d5fd70debca5
aniculescu2 <aniculescu2@gmail.com>
2025-05-31 13:39:46 -0700

    Added potion base

---
+++ potion.gd
@@ -0,0 +1,23 @@
+## Potion class that extends Element2D
+class_name Potion
+extends Element2D
+
+
+enum PotionType {
+    HEALTH,
+    MANA,
+    STRENGTH,
+    SPEED,
+    UNKNOWN
+}
+## Preset type of potion
+@export var potion_type: PotionType = PotionType.UNKNOWN
+## Amount of potion to be applied
+@export var potion_amount: int = 0
+## Duration of potion effect in rounds
+@export var potion_duration: int = 0
+## Effect description of the potion
+@export var potion_effect: String
+func _ready() -> void:
+    print("Potion ready: Type: %s, Amount: %d, Duration: %d seconds" % [PotionType.find_key(potion_type), potion_amount, potion_duration])
+    element_id = Element2D.CellType.ITEM
